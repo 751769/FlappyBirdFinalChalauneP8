35005edb4f3f689c9f821ca4b2bc168aa44fb9af
Prashant <751769@student.dusd.net>
2023-12-21 10:46:46 -0800

    hahaha
    Done buddy

--- FlappyBirdFinalChalaune/Assets/Scripts/GameControl.cs
+++ FlappyBirdFinalChalaune/Assets/Scripts/GameControl.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
+    public TextMeshProUGUI ScoreText;
+    //public Text scoreText;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
 
+    private int score = 0;
+    AudioSource audiosource;
+    public AudioClip scoreSound;
+
+
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
     void Awake ()
     {
         if (instance == null)
@@ -32,9 +47,25 @@ public class GameControl : MonoBehaviour
         }
     }
 
-    public void BirdDied ()
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        ScoreText.text = "Score: " + score.ToString ();
+        PlaySound(scoreSound);
+    }
+
+    public void BirdDied()
     {
         gameOverText.SetActive(true);
         gameOver = true;
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audiosource.PlayOneShot(clip);
     }
 }
